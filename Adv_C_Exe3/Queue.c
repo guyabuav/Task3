10e8f26bad2cb0183c8bc531687ce9c9ddcbf93b
guyabuav <100515287+guyabuav@users.noreply.github.com>
2022-04-23 13:31:47 +0300

    Ass3

--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -1,45 +1,206 @@
 #include "Queue.h"
+#define NULL 0
+
 
 /***************** Queue ADT Implementation *****************/
-
+void reverse(Queue *queue);
 void initQueue(Queue* q)
 {
-	// add your code here
+	q->tail =  NULL; // initialize new queue
+	q->head = NULL;
+	
 }
 
 void destroyQueue(Queue* q)
 {
-	// add your code here
+	intNode* temp = q->head;
+	while (q->head != NULL) {
+		q->head = q->head->next;
+		temp->data = 0; // delete the exist data
+		free(temp); // free the item
+		temp = q->head;
+	}
 }
 
 void enqueue(Queue* q, unsigned int data)
 {
-	// add your code here
+	intNode* New = (intNode*)malloc(sizeof(intNode));
+	if (New == NULL)
+		printf("allocation failed");
+	New->data = data;
+	New->next = NULL;
+	if (q->head == NULL) { // if the queue is empty
+		q->head = New;
+		q->tail = New;
+	}
+	else // if there is items in the queue
+	{
+		q->tail->next = New;
+		q->tail = New;
+	}
 }
 
 unsigned int dequeue(Queue* q)
 {
-	// add your code here
+	int tempd = q->head->data;
+	intNode* temp = q->head;
+	if (q->head == q->tail) { // if there is only one item in queue
+		q->head = NULL;
+		q->tail = NULL;
+	}
+	else {
+		// if there are more than one items in queue
+		q->head = q->head->next;
+	}
+
+
+	free(temp); // free the item the was removed
+	return tempd;
 }
 
 int isEmptyQueue(const Queue* q)
 {
-	// add your code here
+	if (q->head == NULL) // if the queue is empty return 1, else return 0
+		return 1;
+	else return 0;
 }
 
 /***************** Functions using Queues - Implementation/definition **************************/
 
 void rotateQueue(Queue* q)
 {
-	// add your code here
+	int NumOfItems = 1;
+	if (q == NULL) { // is queue is empty
+		printf("There is no queue");
+		return;
+	}
+	intNode* temp1 = q->head;
+	intNode* temp2 = q->tail;
+	intNode* temp3 = NULL;
+	while (temp1 != temp2) {
+		temp1 = temp1->next;
+		if (temp1->next->next == NULL) { // save the one before last item.
+			temp3 = temp1;
+			break;
+		}
+	}
+	q->tail->next = q->head;
+	q->head = q->tail;
+	q->tail = temp3;
+	q->tail->next = NULL;
 }
 
 void cutAndReplace(Queue* q)
 {
-	// add your code here
+	Queue* NewQ1 = (Queue*)malloc(sizeof(Queue));
+	if (NewQ1 == NULL) 
+		printf("allocation failed");
+	initQueue(NewQ1);
+	intNode* temp1 = q->head;
+	intNode* temp2 = q->tail;
+	int QueueCounter = 0; 
+	int QueueDataSum = 0; 
+	while (q->head != 0) { // count the queue items in queue and the sum of data.
+		QueueDataSum += q->head->data;
+		q->head = q->head->next;
+		QueueCounter++;
+	}
+	q->head = temp1;
+	q->tail = temp2;
+	if (QueueCounter % 2 != 0) { // if the queue items are odd - enqueue more item.
+		enqueue(q, (QueueDataSum / QueueCounter));
+		QueueCounter++;
+	}
+	for (int i = 0; i < (QueueCounter / 2); i++) { // seperate queue by 2
+		enqueue(NewQ1, dequeue(q));
+	}
+	reverse(q); // reverse half of the q
+	q->tail->next = NewQ1->head; // connect between 2 new queues
+	q->tail = NewQ1->tail;
+	free(NewQ1); 
 }
 
 void sortKidsFirst(Queue* q)
 {
-	// add your code here
+	intNode* temp1 = q->head;
+	intNode* temp2 = q->tail;
+	intNode* temp3 = (intNode*)malloc(sizeof(intNode));
+	if (temp3 == NULL)
+		printf("allocation failed");
+	int min = q->head->data;
+	int QueueCounter = 0;
+	Queue* NewQ = (Queue*)malloc(sizeof(Queue));
+	initQueue(NewQ);
+	while (q->head != 0) { // count the queue items in queue
+		q->head = q->head->next;
+		QueueCounter++;
+	}
+	q->head = temp1;
+	q->tail = temp2;
+	for (int j = 0; j < QueueCounter; j++) {
+		for (int i = 0; i < QueueCounter; i++) {
+			if (q->head->data < min && q->head->data != 0) { //find the lowest data and put him in newq
+				min = q->head->data;
+				temp3 = q->head;
+			}
+			q->head = q->head->next;
+
+		}
+		temp3->data = 0;
+		enqueue(NewQ, min); // put in the newq the items be order
+		q->head = temp1;
+		q->tail = temp2;
+		if (q->head->data == NewQ->tail->data) {
+			q->head->data = 0;
+			min = 1000;
+		}
+		else
+			min = q->head->data;
+
+	}
+	q->head = NewQ->head;
+	free(temp3);
+}
+
+
+//move the head  to tail
+void RotateQueue2(Queue* q) {
+	intNode* temp1 = q->head;
+	intNode* temp2 = q->tail;
+	intNode* temp3 = NULL;
+	while (temp1 != temp2) {
+		temp1 = temp1->next;
+		if (temp1->next->next == NULL) { // save the one before last item
+			temp3 = temp1;
+			break;
+		}
+	}
+	q->tail->next = q->head;
+	q->head = q->head->next;
+	q->tail = q->tail->next;
+	q->tail->next = NULL;
+
+}
+
+void reverse(Queue *queue) {
+	int element;
+	if (isEmptyQueue(queue)) { // if the queue is empty, return
+		return;
+	}
+	element = dequeue(queue);
+	reverse(queue);
+	enqueue(queue, element);
+}
+
+
+void printQueue(Queue* q) {
+	intNode* temp1 = q->head;
+	intNode* temp2 = q->tail;
+	int inter = 1;
+	while (temp1 != NULL) {
+		printf("the %d item is: %d\n", inter, temp1->data);
+		temp1 = temp1->next;
+		inter++;
+	}
+
 }
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,46 +1,177 @@
 #include "Stack.h"
+#define NULL 0
 
 /***************** Stack ADT Implementation *****************/
 
 void initStack(Stack* s)
 {
-	// add your code here
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	charNode* temp = s->head;
+	while (s->head != NULL) {
+		s->head = s->head->next;
+		temp->data = NULL; // delete the data
+		free(temp);
+		temp = s->head;
+	}
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* newVar = (charNode*)malloc(sizeof(charNode));
+	if (newVar == NULL)
+		printf("allocation failed");
+	if (s->head == NULL) {
+		s->head = newVar;
+		newVar->next = NULL;
+		newVar->data = data;
+	}
+	else {
+		newVar->data = data;
+		newVar->next = s->head;
+		s->head = newVar;
+	}
+	
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	char tempo;
+	tempo = s->head->data;
+	charNode* temp = s->head;
+	s->head = s->head->next;
+	temp->data = NULL;
+	free(temp);
+	temp = NULL;
+	return tempo;
 }
 
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	if (s->head == NULL)
+		return 1;
+	else return 0;
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
 
 void flipBetweenHashes(const char* sentence)
 {
-	// add your code here
+	Stack* HelpS = (Stack*)malloc(sizeof(Stack));
+	if (HelpS == NULL)
+		printf("allocation failed");
+	int i = 0, counter = 0;
+	while (sentence[i] != '\0') {
+		if (sentence[i] != '#')
+			printf("%c", sentence[i]);
+		else {
+			counter++; // count the number of #
+			i++;
+			int intemp = 0;
+			while (sentence[i] != '#') {
+				push(HelpS, sentence[i]); // Push the items between 2 asterisks to new stack
+				i++;
+				intemp++; // count how much letters between 2 asterisks
+			}
+			counter++; // Here we get to the second asterisk
+			if (counter % 2 == 0 && counter != 0) { // if you get to another asterisks (the second) - start print 
+				while (intemp != 0) {
+					printf("%c", HelpS->head->data);
+					pop(HelpS);
+					intemp--;
+				}
+			}
+			
+		}
+		i++;
+		
+	}
+	free(HelpS);
 }
 
 int isPalindrome(Stack* s)
 {
-	// add your code here
+	if (s->head == NULL) 
+		return 1;
+
+	int size = 0, counter = 1;
+	charNode* temp1 = s->head;
+	charNode* temp2 = s->head;
+
+	while (temp1 != NULL) {
+		size++;
+		temp1 = temp1->next;
+	}
+	temp1 = s->head;
+	for (int i = 1; i <= (size + 1) / 2; i++) {
+		for (int j = 1; j < size; j++) {
+			temp2 = temp2->next;
+		}
+		if (temp2->data != temp1->data) {
+			return 0;
+		}
+		temp1 = temp1->next;
+		size -= 2;
+		temp2 = temp1;
+		if (size == 1 || size == 0) {
+			return 1;
+
+		}
+
+	}
 }
 
 void rotateStack(Stack* s, int n)
 {
-	// add your code here
+	if (n < 0 || s->head == NULL)
+		return;
+	Stack* temp1 = (Stack*)malloc(sizeof(Stack));
+	Stack* temp = (Stack*)malloc(sizeof(Stack));
+	if (temp == NULL || temp1 == NULL)
+		printf("allocation failed");
+	initStack(temp1);
+	initStack(temp);
+	charNode* temp2 = s->head;
+	temp->head = s->head;
+	int count = 0;
+	while (temp2 != NULL) {
+		count++;
+		temp2 = temp2->next;
+	}
+	if (n > count)
+	{
+		return;
+	}
+	for (int i = 0; i < count - n; i++) // cut the stack by count - n letters to new stack
+	{
+		char tav = pop(s);
+		push(temp, tav);
+	}
+	for (int i = 0; i < n; i++) //  cut the stack by n letters to new stack
+	{
+		char tav = pop(s);
+		push(temp1, tav);
+	}
+
+	for (int i = 0; i < count - n; i++) // push the items to stack by order
+	{
+		char tav = pop(temp);
+		push(s, tav);
+	}
+	for (int i = 0; i < n; i++) //push the items to stack by order
+	{
+		char tav = pop(temp1);
+		push(s, tav);
+	}
+	while (s->head != NULL) // print new stack
+	{
+		printf("%c\n", s->head->data);
+		s->head = s->head->next;
+	}
+	free(temp);
+	free(temp1);
 }
